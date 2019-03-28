b3a54ccbce93a9742b5779ce7553b0e7787ce8ce
coldsewoo <coldsewoo@gmail.com>
2019-03-28 12:33:43 +0900

    uniquesubstrings

--- Unique_substrings.js
+++ Unique_substrings.js
@@ -1,19 +1,28 @@
-var getSubstrings = function(str) {
-  str = str.toString();
-  let strArr = str.toLowerCase().split('');
-  let resultArr = []; 
-  // insert your code here!
-  for (let i = 0; i < strArr.length; i++)
-  {
-    for(let j = strArr.length; j > i; j--) {
-      resultArr.push(strArr.slice(i,j).join(''));
-    }
-  }
-  return resultArr.reduce((p,c) => {
-    if(p.indexOf(c) == -1) p.push(c);
-    return p
-  },[]).length
-};
+// var getSubstrings = function(str) {
+//   str = str.toString();
+//   let strArr = str.toLowerCase().split('');
+//   let resultArr = []; 
+//   // insert your code here!
+//   for (let i = 0; i < strArr.length; i++)
+//   {
+//     for(let j = strArr.length; j > i; j--) {
+//       resultArr.push(strArr.slice(i,j).join(''));
+//     }
+//   }
+//   return resultArr.reduce((p,c) => {
+//     if(p.indexOf(c) == -1) p.push(c);
+//     return p
+//   },[]).length
+// };
 
 
-getSubstrings("YOLO")
+// getSubstrings("YOLO")
+
+Array.prototype.something = function (...args) {
+  let result = this;
+  result = this.concat(args)
+  return {result, result.length};
+}
+
+var a = [1,2,3];
+a.something('b','c','d','e'); // ?
